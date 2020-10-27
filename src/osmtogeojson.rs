dfdcd7ac8369b649fe7e25b6854944db30a0cd77
Edouard Poitras <edouardpoitras@gmail.com>
2020-10-26 23:48:57 -0400

    Better error handling for converting osm to geojson

--- src/osmtogeojson.rs
+++ src/osmtogeojson.rs
@@ -1,6 +1,18 @@
 use std::fs::File;
 use std::io::Write;
 use std::process::Command;
+use std::{error::Error, fmt};
+
+#[derive(Debug)]
+struct ConvertError;
+
+impl Error for ConvertError {}
+
+impl fmt::Display for ConvertError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Failed to convert osm file to geojson file - ensure you have the osmtogeojson tool installed (npm install -g osmtogeojson)")
+    }
+}
 
 pub fn convert_osm_to_geojson(
     osm_file: String,
@@ -8,13 +20,23 @@ pub fn convert_osm_to_geojson(
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut file;
     if let Result::Ok(output) = Command::new("osmtogeojson").arg(&osm_file).output() {
-        file = File::create(geojson_file)?;
-        write!(file, "{}", String::from_utf8_lossy(&output.stdout))?;
+        file = match File::create(geojson_file) {
+            Result::Ok(f) => f,
+            Result::Err(err) => return Err(Box::new(err)),
+        };
+        match write!(file, "{}", String::from_utf8_lossy(&output.stdout)) {
+            Result::Ok(_) => return Ok(()),
+            Result::Err(err) => return Err(Box::new(err)),
+        };
     } else if let Result::Ok(output) = Command::new("osmtogeojson.cmd").arg(&osm_file).output() { // Windows
-        file = File::create(geojson_file)?;
-        write!(file, "{}", String::from_utf8_lossy(&output.stdout))?;
-    } else {
-        panic!("failed to convert osm file to geojson file - ensure you have the osmtogeojson tool installed (npm install -g osmtogeojson)");
+        file = match File::create(geojson_file) {
+            Result::Ok(f) => f,
+            Result::Err(err) => return Err(Box::new(err)),
+        };
+        match write!(file, "{}", String::from_utf8_lossy(&output.stdout)) {
+            Result::Ok(_) => return Ok(()),
+            Result::Err(err) => return Err(Box::new(err)),
+        };
     }
-    Ok(())
+    Err(Box::new(ConvertError))
 }
